b122969bf19d7d96230135ea785cd41bc41582d1
Vishwanath R <vishwanath@LogicValley.in>
2020-10-14 16:53:56 +0530

    Refactored & Included comments forwebpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), //Output build directory name
+    publicPath: "/", //The bundled files will be available in the browser under this path.
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(), //Remove HTML file and re-create on each build
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
+      template: path.resolve(__dirname, "src/index.html"),
     }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: "async",
+    }), //Load bundle scripts asynchronously
   ],
   devServer: {
-    contentBase: './src',
-    inline: true,
-    hot: true,
+    open: true, //To open the browser after server had been started
+    contentBase: './src', //where to serve content(static files) from,
+    inline: true, //Inject scripts into the bundle to show live reloading and build messages in the browser console.
+    hot: true, //Enables HMR
+    historyApiFallback: true, //In case 404 responses, root(index.html) file will be served,
   },
+  devtool: "source-map", //How source codes are mapped/shown in the browser
   module: {
     rules: [
       {
