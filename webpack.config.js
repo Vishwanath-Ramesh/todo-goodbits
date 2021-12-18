29055f105baab96cf7355a3755571c4e7c031d01
Vishwanath-Ramesh <vishwanathr.dev@outlook.com>
2021-12-18 14:07:16 +0530

    Upgraded webpack from v4 to v5

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = (env) => ({
   mode: env.NODE_ENV || 'development',
-  entry: './src/index.js',
+  // entry: './src/index.js', // From v5, this is default
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].chunk.js',
-    path: path.resolve(__dirname, 'dist'), // Output build directory name
-    publicPath: '/', // The bundled files will be available in the browser under this path.
+    // path: path.resolve(__dirname, 'dist'), // Output build directory name. From v5, this is default
+    publicPath: '/', // The bundled files will be available in the browser under this path. Eg. A request to a chunk will look like /1.chunk.js. In our case('/'), it's relative to HTML page
   },
   plugins: [
     new CleanWebpackPlugin(), // Remove HTML file and re-create on each build
@@ -26,8 +26,11 @@ module.exports = (env) => ({
     // host: '0.0.0.0', // Uncomment this, If you want your server to be accessible externally(Mobile etc.)
     port: 3000, // Port number to listen for requests
     open: true, // To open the browser after server had been started
-    contentBase: path.join(__dirname, './public'), // where to serve content(static files) from,
-    inline: true, // Inject scripts into the bundle to show live reloading and build messages in the browser console.
+    static: {
+      directory: path.join(__dirname, './public'), // where to serve content(static files) from,
+      publicPath: '/', // Tell the server at which URL to serve static.directory content. For example to serve a file from /public/assets/manifest.json at /static/manifest.json, this value should be '/static/'
+    },
+    // inline: true, // Inject scripts into the bundle to show live reloading and build messages in the browser console. Webpack removed this in v5.
     hot: true, // Enables HMR
     historyApiFallback: true, // In case 404 responses, root(index.html) file will be served,
   },
